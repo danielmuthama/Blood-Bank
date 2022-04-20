3c1b43364e0e9b0b5189782de49991a1c8d71893
davidkiama <davidkiama98@gmail.com>
2022-04-21 01:02:20 +0300

    Added funtionality to apply to donate

--- donor/models.py
+++ donor/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 
@@ -10,6 +9,20 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class BloodRequest(models.Model):
+    user = models.ForeignKey(
+        User, related_name="user_id", on_delete=models.CASCADE)
+    blood_group = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
+
+
+class BloodDrive(models.Model):
+    user = models.ForeignKey(
+        User, related_name="user_id", on_delete=models.CASCADE)
+    blood_group = models.CharField(max_length=255)
+    amount_wanted = models.IntegerField(default=0)
 
 
 class Donor(models.Model):
--- home/views.py
+++ home/views.py
@@ -1,19 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from donor.models import Donor, Hospital, BloodRequest, BloodDrive
 
 # Create your views here.
+
+
+# Helper function to request blood
+
+def request_blood(request):
+    # Grab the donor's email
+    # Grab the hospital's email
+    # Construct the email with the blood type and hospital contact info
+    # Send the email to the donor
+
+    pass
 
 
 def home(request):
     return render(request, 'home.html')
 
 
-def hos_request_blood(request):
-    return render(request, 'hos/request_blood.html')
-
-
 def hos_create_blood_drive(request):
+    # Check if authenticated user is a hospital user obj
+    hospital = Hospital.objects.get(user=request.user)
+    if not hospital:
+        return redirect('home')
+
+    if request.method == 'POST':
+        user = request.user
+        blood_group = request.POST['blood_group']
+        amount_wanted = request.POST['amount_wanted']
+
+        blood_drive = BloodDrive(
+            user=user, blood_group=blood_group, amount_wanted=amount_wanted)
+        blood_drive.save()
+
+        return redirect(home)
+
     return render(request, 'hos/reate_blood_drive.html')
 
 
 def don_apply_to_donate(request):
+    # Check if authenticated user is a donor user obj
+    donor = Donor.objects.get(user=request.user)
+    if not donor:
+        return redirect('home')
+
+    if request.method == 'POST':
+        user = request.user
+        blood_group = request.POST['blood_group']
+        age = request.POST['age']
+
+        blood_request = BloodRequest(
+            user=user, blood_group=blood_group, age=age)
+        blood_request.save()
+
+        return render(request, 'home.html')
     return render(request, 'don/apply_to_donate.html')
