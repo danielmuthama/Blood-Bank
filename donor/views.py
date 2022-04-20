6b56dddbab54452d8a2d76bc9f1a42405ad16be5
davidkiama <davidkiama98@gmail.com>
2022-04-20 12:29:21 +0300

    Listed the main views

--- donor/views.py
+++ donor/views.py
@@ -1,2 +1,32 @@
 from django.shortcuts import render
 from .models import Donor, Hospital, User
+
+
+''''
+
+Views to be rendered as API endpoints
+
+'''
+
+
+''''
+
+Views to handle requests from the front end
+
+'''
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def hos_request_blood(request):
+    return render(request, 'hos/request_blood.html')
+
+
+def hos_create_blood_drive(request):
+    return render(request, 'hos/reate_blood_drive.html')
+
+
+def don_apply_to_donate(request):
+    return render(request, 'don/apply_to_donate.html')
