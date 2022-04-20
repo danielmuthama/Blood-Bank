0bd433701cfee5059f58c731a8aadb1e442cb116
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-20 12:10:43 +0300

    add class serializers

---
+++ donor/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Donor,Hospital,User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields ='__all__'
+class HospitalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hospital
+        fields ='__all__'
+class DonorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donor
+        fields ='__all__'                
--- donor/views.py
+++ donor/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
+from .models import Donor,Hospital,User
 
-# Create your views here.
