2cc119929e3c0ab2b1528483b60d3c5ff83f1c79
kojwangbora <kojwangbora254@gmail.com>
2022-04-21 12:05:46 +0300

    add postgres database and create the user model

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'Blood_Bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blood_bank',
+        'USER': 'moringa',
+    'PASSWORD':'Access'
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+AUTH_USER_MODEL = 'home.User'
